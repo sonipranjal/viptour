487f7a4afc318dbeee6d7f42ad7b8f6a923aee8b
pranjal soni <pranjalsoni6256@gmail.com>
2019-10-26 14:21:22 +0530

    this is my first web developement frontend project

---
+++ php/index.php
@@ -0,0 +1,67 @@
+<?php
+
+    $error = ""; $successMessage = "Message sent";
+     echo $error.$successMessage; 
+
+    if ($_POST) {
+        
+        if (!$_POST["email"]) {
+            
+            $error .= "An email address is required.<br>";
+            
+        }
+        
+        if (!$_POST["content"]) {
+            
+            $error .= "The content field is required.<br>";
+            
+        }
+        
+        if (!$_POST["subject"]) {
+            
+            $error .= "The subject is required.<br>";
+            
+        }
+        
+        if ($_POST['email'] && filter_var($_POST["email"], FILTER_VALIDATE_EMAIL) === false) {
+            
+            $error .= "The email address is invalid.<br>";
+            
+        }
+        
+        if ($error != "") {
+            
+            $error = '<div class="alert alert-danger" role="alert"><p>There were error(s) in your form:</p>' . $error . '</div>';
+            
+        } else {
+            
+            $emailTo = "pranjalsoni95@gmail.com";
+            
+            $subject = $_POST['subject'];
+            
+            $content = $_POST['content'];
+            
+            $headers = "From:  ".$_POST['email'];
+            
+            if (mail($emailTo, $subject, $content, $headers)) {
+                
+                $successMessage = '<div class="alert alert-success" role="alert">Your message was sent, we\'ll get back to you ASAP!</div>';
+                
+                
+            } 
+            
+            else {
+                
+                $error = '<div class="alert alert-danger" role="alert"><p><strong>Your message couldn\'t be sent - please try again later</div>';
+                
+                
+            }
+            
+        }
+        
+        
+        
+    }
+
+?>
+
